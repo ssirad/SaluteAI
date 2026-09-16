032c421738331e045a29c120c618335beec2ff0b
ssirad <105271868+ssirad@users.noreply.github.com>
2026-09-16 11:41:45 +0200

    Update patterns_elissa_salute.py
    all comments in english - to finish the patterns still

--- patterns_elissa_salute.py
+++ patterns_elissa_salute.py
@@ -4,14 +4,14 @@ patterns_symptoms = {
         #others
     ],
     r'.*(stomach pain|abdominal pain|belly pain|abdominal discomfort|stomach ache).*': [
-        'risposta del mal di stomaco' #da cambiare
+        'risposta del mal di stomaco' #change
     ],
     r'.*(neck pain|neck tension|cervical pain|neck ache).*': [
         'Can you describe better the position in the neck you feel pain? (like front, back, right, left)' #da cambiare
         #others
     ],
     r'.*(shoulder pain|shoulder tension|tight shoulders).*': [
-        'How bad if the shoulder pain?' #da cambiare
+        'How bad if the shoulder pain?' #change
         #others
     ],
     r'.*(chest pain|heart pain|chest pressure|chest tightness|heaviness in the chest).*': [
@@ -19,40 +19,40 @@ patterns_symptoms = {
         #others
     ],
     r'.*(muscle pain|muscle ache|muscle tension|muscular discomnfort|myalgia|body aches).*': [
-        'In what body part do you feel the muscle discomfort?' #da cambiare
+        'In what body part do you feel the muscle discomfort?' #change
         #others
     ],
     r'.*(joint pain|stiffness|soreness|joint discomnfort|joint ache).*': [
-        'Do you have walking problems caused by the joint discomfort?' #da cambiare
+        'Do you have walking problems caused by the joint discomfort?' #change
         #others
     ],
     r'.*(sweating|perspiration|hot flush|facial flushing|sudden warmth|cold sweat).*': [
-        'How long did the perspiration was?' #da cambiare
+        'How long did the perspiration was?' #change
         #others
     ],
     r'.*(breathing difficulty|difficulty breathing|truble breathing|short breath|breathless|struggling to breathe|restricting breathing).*': [
-        'How long did the heart racing last?' #da cambiare
+        'How long did the heart racing last?' #change
         #others
     ],
     r'.*(heart palpitations|racing heart|hearth racing|pounding heart|fluttering heart).*': [
-        'How long did the heart racing last?' #da cambiare
+        'How long did the heart racing last?' #change
         #others
     ],
-#DA FINIRE ANCORA NE MANCANO UN PO - PRENDI LE COSE DA SCRIVERE DA QUA https://chatgpt.com/c/6aa941cc-376c-83eb-bbd9-c97d72cb2175
+#SILL HAVE TO PUT PATTERNS - https://chatgpt.com/c/6aa941cc-376c-83eb-bbd9-c97d72cb2175
 }
 patterns_situations = {
     r'.*(headache).*': [
-        'risposta del mal di testa' #da cambiare
+        'risposta del mal di testa' #change
     ],
     r'.*(stomach pain).*': [
-        'risposta del mal di stomaco' #da cambiare
-    ]} #da vedere perchè non penso che si possa fare
+        'risposta del mal di stomaco' #change
+    ]}
 
 patterns_emotions = {
     r'.*(headache).*': [
-        'risposta del mal di testa' #da cambiare
+        'risposta del mal di testa' #change
     ],
     r'.*(stomach pain).*': [
-        'risposta del mal di stomaco' #da cambiare
+        'risposta del mal di stomaco' #change
     ]
-} #da finire
+} #not finished
